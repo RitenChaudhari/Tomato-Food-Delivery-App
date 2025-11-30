b49ab7cc630d2065052da85eee599531afa4de88
RitenChaudhari <ritenchaudhari@gmail.com>
2025-11-30 17:15:06 +0530

    Initial Commit

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/frontend_assets/assets'
+const ExploreMenu = () => {
+  return (
+    <div className='explore-menu' id='explore-menu'>
+        <h1>Explore our Menu</h1>
+        <p></p>
+    </div>
+  )
+}
+
+export default ExploreMenu
