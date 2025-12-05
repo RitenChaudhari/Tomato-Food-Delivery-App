de3ed3f8449e24753031589fa24dbc51f06561cb
RitenChaudhari <ritenchaudhari@gmail.com>
2025-12-05 21:44:45 +0530

    Added Footer

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { assets } from '../../assets/frontend_assets/assets'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+            <img src={assets.logo} alt=''/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio animi asperiores, architecto harum, quasi consequuntur dolorum veritatis illo deleniti error magni ea minus sint? Rem distinctio sed enim mollitia molestiae.</p>
+            <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Delivery</li>
+                <li>Privacy Policy</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>Get In Touch</h2>
+            <ul>
+                <li>+1-21-456-1234</li>
+                <li>contact@tomato.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className='footer-copyright'>Copyright  {new Date().getFullYear()} &copy; Tomato.com-All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
